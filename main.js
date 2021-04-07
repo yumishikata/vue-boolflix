9c420fb0f94fc52bfbd555a5f3a161b45536996a
Yumi Shikata <yumishika17@gmail.com>
2021-04-07 11:56:03 +0200

    Milestone 2

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ var app = new Vue ({
     el:'#root',
     data: {
         cerca: '',
-        films: []
+        films: [],
+        shows: []
     },
     methods: {
         search: function(){
@@ -10,6 +11,17 @@ var app = new Vue ({
             .then(risultato => {
                 this.films = risultato.data.results;
                 console.log(this.films);
+                this.films.forEach(element => {
+                    element.vote_average = Math.ceil(parseInt(element.vote_average)/2);
+                });
+            })
+            axios.get('https://api.themoviedb.org/3/search/tv?api_key=d04f615a69c6c05ddd2d3983ab546aee&query=' +this.cerca)
+            .then(risultato => {
+                this.shows = risultato.data.results;
+                console.log(this.shows);
+                this.shows.forEach(element => {
+                    element.vote_average = Math.ceil(parseInt(element.vote_average)/2);
+                });
             })
         }
     },
